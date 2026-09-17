ed09e6295ede2599a71b4b1d0be77b8c073db6de
Shuto0577 <shuto.17_0707@icloud.com>
2026-09-17 17:09:25 +0900

    フォーマット修正

--- Assets/_Project/Features/Enemy/Scripts/TestEnemyStates.cs
+++ Assets/_Project/Features/Enemy/Scripts/TestEnemyStates.cs
@@ -33,10 +33,7 @@ namespace CreativeAI.Gameplay
         public TestEnemyIdleState(TestEnemyController core)
             : base(core) { }
 
-        public override void Enter()
-        {
-            Debug.Log("待機ステート開始");
-        }
+        public override void Enter() { }
 
         public override void Update()
         {
@@ -47,10 +44,7 @@ namespace CreativeAI.Gameplay
             }
         }
 
-        public override void Exit()
-        {
-            Debug.Log("待機ステート終了");
-        }
+        public override void Exit() { }
     }
 
     /// <summary>
